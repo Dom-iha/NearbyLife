4a7e629b61f7e71fb3f49c75811bde281062a172
Dom-iha <taqib.ibrahim@gmail.com>
2023-04-05 14:58:27 +0100

    splashscreen

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -2,8 +2,8 @@
 
 const SplashScreen = () => {
    return (
-      <section className="grid place-items-center to-blue-300">
-         <h1 className="">NearByLife</h1>
+      <section className="grid min-h-screen place-items-center bg-slate-300 to-blue-300">
+         <h1 className="text-sky-600 text-3xl text-weight">NearByLife</h1>
       </section>
    );
 };
